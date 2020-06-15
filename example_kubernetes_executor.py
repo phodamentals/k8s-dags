9e457ca7720f6633b4abb5d442023c0f867187ff
phodamentals <phodamentals@gmail.com>
2020-06-14 21:51:46 -0500

    push

--- example_kubernetes_executor.py
+++ example_kubernetes_executor.py
@@ -1,7 +1,7 @@
 import os
 
 from airflow import DAG
-from airflow.example_dags.libs.helper import print_stuff
+# from airflow.example_dags.libs.helper import print_stuff
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
