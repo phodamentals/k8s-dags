c82c19ba87463cd04865fd6e94be74d84491ce8a
phodamentals <phodamentals@gmail.com>
2020-07-03 15:42:48 -0500

    import PythonOperator

--- test_k8s_exec_py_operator.py
+++ test_k8s_exec_py_operator.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
 default_args = {
     'owner': 'airflow',
