cb3a8fb258bfa1040260ea18967a0be439d9de9b
phodamentals <phodamentals@gmail.com>
2020-08-23 19:00:57 -0500

    stress test

--- test_k8s_pod_operator.py
+++ test_k8s_pod_operator.py
@@ -81,7 +81,7 @@ task3 = KubernetesPodOperator(
 stress = KubernetesPodOperator(
     namespace='default',
     image="alpine:latest",
-    cmds="ping 0 -c 1000",
+    cmds=['ping 0 -c 1000'],
     # cmds=["fulload() { dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null & }; fulload; read; killall dd"],
     # arguments=[""],
     # labels={"foo": "bar"},
